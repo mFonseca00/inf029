f09242a8156770151dcd68a6d1f76a4de0e56ce7
Marcus <mv070900@gmail.com>
2024-11-19 17:28:38 -0300

    Arquivo criado

--- Treinos/prova_2017-2.c
+++ Treinos/prova_2017-2.c
@@ -1,1 +1,149 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define maxTam 10 //Tamanho máximo do vetor de caracteres
+
+typedef struct {
+    int posAtual;
+    char vetor[maxTam];
+    int repeticoes[maxTam];
+    char retorno[100];
+    char caractereTrocado;
+}vetorChar;
+
+void insereCaractere(vetorChar caracteres){
+    if(caracteres.posAtual < maxTam){
+        printf("Insira o caractere a ser adicionado no vetor: ");
+        scanf("%c", &caracteres.vetor[caracteres.posAtual]);
+        caracteres.posAtual++;
+        strcpy(caracteres.retorno,"Caractere inserido\n");
+    }
+    else{
+        strcpy(caracteres.retorno,"O caractere nao pode ser inserido\n");
+    }
+}
+
+int trocaCaractere(vetorChar caracteres){
+    char novo;
+    printf("Informe o novo caractere, para realizar a troca: ");
+    scanf("%c",&novo);
+    for(int i=0; i < caracteres.posAtual+1; i++){
+        if(caracteres.vetor[i]>novo){
+            caracteres.caractereTrocado = caracteres.vetor[i];
+            caracteres.vetor[i]=novo;
+            strcpy(caracteres.retorno, "Caractere alterado\n");
+            return 1;
+        }
+    }
+    strcpy(caracteres.retorno, "O caractere nao pode ser alterado\n");
+    return 0;
+}
+
+void removerCaractere(vetorChar caracteres){
+    caracteres.posAtual--;
+    strcpy(caracteres.retorno, "Caractere removido\n");
+}
+
+void listarCaracteres(vetorChar caracteres){
+    printf("Lista de caracteres: ");
+    for(int i=0; i < strlen(caracteres.vetor); i++){
+        printf("%c",caracteres.vetor[i]);
+        if(i<strlen(caracteres.vetor)-1){
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+void listarDistintos(vetorChar caracteres){
+    vetorChar Distintos;
+    int contagem;
+    Distintos.posAtual=0;
+
+    for(int i=0; i < maxTam; i++){
+        //Verificação se o caracter já se encontra listado como distinto
+        bool naoListado=true;
+        for(int j=0; j<strlen(Distintos.vetor);j++){
+            if(caracteres.vetor[i]==Distintos.vetor[j]){
+                naoListado=false;
+            }
+        }
+        //Caso não esteja listado, contabilizar repetições no vetor base e adicionar como distinto
+        if(naoListado){
+            contagem=0;
+            for(int j=0; j < strlen(caracteres.vetor); j++){
+                if(caracteres.vetor[j]==caracteres.vetor[i]){
+                    contagem++;
+                }
+            }
+            Distintos.repeticoes[Distintos.posAtual] = contagem;
+            Distintos.vetor[Distintos.posAtual]=caracteres.vetor[i];
+            Distintos.posAtual++;
+            Distintos.vetor[Distintos.posAtual]='\0';
+        }
+    }
+    
+    //Listagem do vetor contentdo todos os caracteres distintos
+    printf("Lista de caracteres distintos: ");
+    for(int i=0; i<strlen(Distintos.vetor); i++){
+        printf("%c: %d",Distintos.vetor[i],Distintos.repeticoes[i]);
+        if(i<strlen(Distintos.vetor-1)){
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main(void){
+
+    vetorChar caracteres;
+    caracteres.posAtual=0;
+    caracteres.vetor[0]='\0';
+    int opcao=0;
+    
+    printf("Digite a opcao desejada:\n'1' - inserir caractere\n'2' - trocar caractere\n'3' - remover caracteres\n"
+    "'4' - listar todos os caracteres\n'5' - listar caracteres distintos\n'6' - Sair");
+    while(opcao!=6){
+        scanf("%d",&opcao);
+        switch (opcao){
+            case 1:{
+                inserirCaractere(caracteres);
+                printf("%s",caracteres.retorno);
+                break;
+            }
+            case 2:{
+                int trocou = 0;
+                trocou=trocaCaractere(caracteres);
+                printf("%s",caracteres.retorno);
+                if(trocou == 1){
+                    printf("Caractere removido: ", caracteres.caractereTrocado);
+                }
+                break;
+            }
+            case 3:{
+                removerCaractere(caracteres);
+                printf("%s",caracteres.retorno);
+                break;
+            }
+            case 4:{
+                listarCaracteres(caracteres);
+                break;
+            }
+            case 5:{
+                listarDistintos(caracteres);
+                break;
+            }
+            case 6:{
+                opcao = 6;
+                break;
+            }
+            default:{
+                printf("Opção inválida, tente novamente.\n");
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
