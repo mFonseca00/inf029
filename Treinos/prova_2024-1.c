990818635fd59fe77527d234d4a762203da6d81a
Marcus Fonseca <mv070900@gmail.com>
2024-11-26 12:39:41 -0300

    Começo funções Q1

--- Treinos/prova_2024-1.c
+++ Treinos/prova_2024-1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define maxPessoas = 3;
 
+typedef struct pessoa{
+    char nome[50];
+    int idade;
+}pessoa;
+
+void lerDados(pessoa pessoas[maxPessoas]){
+    for(int i=0; i<maxPessoas; i++){
+        printf("Digite o nome da %d pessoa: ", i+1);
+        fgets(pessoas[i].nome,50,stdin);
+        printf("Digite a idade da %d pessoa: ", i+1);
+        scanf("%d",&pessoas[i].idade);
+    }
+}
+
+void impirmirDados(pessoa pessoas)
