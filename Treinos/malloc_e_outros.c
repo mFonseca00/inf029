dba712097f9346cde9c69afef628b54fc9749812
Marcus Fonseca <mv070900@gmail.com>
2024-12-18 13:11:36 -0300

    treino malloc e calloc

--- Treinos/malloc_e_outros.c
+++ Treinos/malloc_e_outros.c
@@ -3,4 +3,31 @@
 
 int main(void) {
 
+    //Malloc
+
+    int *x = malloc(sizeof(int)); // aloca na memória o espaço indicado como parâmetro. retorna um ponteiro casa seja possível alocar e caso não, retorna NULL
+    if(x == NULL){
+        printf("Não foi possível alocar memória!");
+    }
+    else{
+        printf("Memória alocada");
+        *x=50;
+        printf("&y = %p", x);
+        printf("x = %d", *x);
+    }
+
+    //Calloc
+
+    int *y = calloc(1, sizeof(int)); // funciona de maneria semelhante ao malloc, porém permite alocar para uma quantidade de elementos para alocação de memória (útil para arrays)
+    if(y == NULL){
+        printf("Não foi possível alocar memória!");
+    }
+    else{
+        printf("Memória alocada");
+        *y=50;
+        printf("&y = %p", y);
+        printf("y = %d", *y);
+    }
+
+    return 0;
 }
